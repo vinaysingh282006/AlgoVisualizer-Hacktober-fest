e769c8b5ebf5511525363d3c0550f4591af63a24
Kajal Rastogi03 <adityarastogiar0456@gmail.com>
2025-10-09 11:39:25 +0530

    Add Floyd-Warshall Algorithm Page with All-Pairs Shortest Path Visualization

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,8 @@ import StringRabinKarpPage from "./pages/StringRabinKarpPage";
 import PrimPage from "./pages/PrimPage";
 import KruskalPage from "./pages/KruskalPage";
 import HuffmanPage from "./pages/HuffmanPage";
+import FloydWarshallPage from "./pages/GraphFloydWarshall";
+
 
 // Components
 import LinkedListPage from "./components/pages/LinkedListPage";
@@ -216,6 +218,8 @@ const App = () => {
                   <Route path="/prims" element={<PrimPage />} />
                   <Route path="/kruskal" element={<KruskalPage />} />
                   <Route path="/huffman" element={<HuffmanPage />} />
+                  <Route path="/graph/floyd-warshall" element={<FloydWarshallPage />} />
+
 
                   {/* Data Structures Documentation */}
                   <Route path="/data-structures-docs" element={<DSDocumentation />} />
