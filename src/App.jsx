947720602c657dcf72b10f2efb2b6d02df7310bf
Kajal Rastogi03 <adityarastogiar0456@gmail.com>
2025-10-19 22:38:36 +0530

    Add Documentation of Radix Sort

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,7 @@ import BeginnerPrograms from "./pages/BeginnerPrograms";
 import ClosestPair from './pages/ClosestPair';
 
 import ConvexHull from "./pages/ConvexHull";
+import RadixSortDocs from "./pages/RadixSortDocs.jsx";
 
 // Components
 import ArrayVisualizer from "./pages/Array.jsx";
@@ -307,6 +308,8 @@ const App = () => {
                   <Route path="/beginner-programs" element={<BeginnerPrograms />} />
                   <Route path="/closest-pair" element={<ClosestPair />} />
                   <Route path="/convex-hull" element={<ConvexHull />} />
+                  <Route path="/radix-sort-docs" element={<RadixSortDocs />} />
+
 
 
                   {/* Data Structures Documentation */}
