5c12b1673be26cbea3c5cccffc35e3e6604941e0
Kajal Rastogi03 <adityarastogiar0456@gmail.com>
2025-10-19 22:33:25 +0530

    Add Documentation of Quick Sort

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,7 @@ import BeginnerPrograms from "./pages/BeginnerPrograms";
 import ClosestPair from './pages/ClosestPair';
 
 import ConvexHull from "./pages/ConvexHull";
+import QuickSortDocs from "./pages/QuickSortDocs.jsx";
 
 // Components
 import ArrayVisualizer from "./pages/Array.jsx";
@@ -307,6 +308,8 @@ const App = () => {
                   <Route path="/beginner-programs" element={<BeginnerPrograms />} />
                   <Route path="/closest-pair" element={<ClosestPair />} />
                   <Route path="/convex-hull" element={<ConvexHull />} />
+                  <Route path="/quick-sort-docs" element={<QuickSortDocs />} />
+
 
 
                   {/* Data Structures Documentation */}
